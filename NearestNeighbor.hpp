8668ce29697f64dd11b6b4fea29be3f9fb603a8f
dawa-sonam <124377303+dawa-sonam@users.noreply.github.com>
2023-12-22 21:46:14 -0500

    Update NearestNeighbor.hpp

--- NearestNeighbor.hpp
+++ NearestNeighbor.hpp
@@ -18,11 +18,14 @@ Date: December 22, 2023
 #include <algorithm>
 #include <sstream>
 
+// Class representing a node with an ID, latitude, and longitude
 class NODE
 {
 public:
+    // Constructor to initialize NODE with an ID, latitude, and longitude
     NODE(int id, double lat, double lon) : id_(id), lat_(lat), lon_(lon) {}
 
+    // Getter functions to retrieve the ID, latitude, and longitude of the NODE
     int getId() const
     {
         return id_;
@@ -35,15 +38,15 @@ public:
     {
         return lon_;
     }
-    // calculating Euclidean distance between two nodes
-   // Euclidean distance formula
+    
+    // Static method to calculate Euclidean distance between two NODEs
     static double distance(const NODE &a, const NODE &b)
     {
         return std::hypot(a.lat_ - b.lat_, a.lon_ - b.lon_);
     }
 
-
 private:
+    // Private members for node ID, latitude, and longitude
     int id_;
     double lat_;
     double lon_;
@@ -51,7 +54,8 @@ private:
 
 // Function to read nodes from a file and return a vector of NODEs
 std::vector<NODE> read_nodes_from_file(const std::string& filename)
-{
+{   
+    // Open the file
     std::ifstream file(filename);
     if (!file.is_open()) {
         std::cerr << "Error opening file: " << filename << std::endl;
@@ -63,6 +67,7 @@ std::vector<NODE> read_nodes_from_file(const std::string& filename)
     int id;
     double lat, lon;
 
+    // Parse each line in the file to extract ID, latitude, and longitude
     while (std::getline(file, current_line))
     {
         std::istringstream iss(current_line);
@@ -71,18 +76,20 @@ std::vector<NODE> read_nodes_from_file(const std::string& filename)
             parsed_nodes.emplace_back(id, lat, lon);
         }
     }
-
+    // Return the vector of parsed nodes
     return parsed_nodes;
 }
 
 // Function to perform the nearest neighbor algorithm on a set of nodes
 void nearestNeighbor(const std::string &filename)
 {
+    // Read nodes from the file
     auto parsed_nodes = read_nodes_from_file(filename);
     if (parsed_nodes.empty()) {
         return;
     }
 
+    // Initialize data structures to track visited nodes, path, and total distance
     std::vector<bool> visited(parsed_nodes.size(), false);
     std::vector<int> path;
     double total_distance = 0.0;
@@ -91,11 +98,11 @@ void nearestNeighbor(const std::string &filename)
     path.push_back(current_node_it->getId());
     visited[current_node_it - parsed_nodes.begin()] = true;
 
-
     auto start_time = std::chrono::high_resolution_clock::now();
     
+    // Nearest neighbor algorithm loop
     for (size_t i = 1; i < parsed_nodes.size(); ++i)
-        {
+    {
         auto nearest_node_it = parsed_nodes.end();
         double minDistance = std::numeric_limits<double>::max();
 
@@ -113,6 +120,7 @@ void nearestNeighbor(const std::string &filename)
             }
         }
 
+        // Update visited nodes, total distance, and path
         visited[nearest_node_it - parsed_nodes.begin()] = true; 
         total_distance += minDistance;
         path.push_back(nearest_node_it->getId());
@@ -134,6 +142,5 @@ void nearestNeighbor(const std::string &filename)
 
     std::cout << "\nTotal Distance: " << total_distance << "\nExecution time in ms: " << duration.count() << std::endl;
 }
-
 
 #endif // NEAREST_NEIGHBOR_HPP
